a78a02414ea01079bb025c19c54087005ba4a1da
Matt Gregg <mgregg@nerdery.com>
2021-02-03 09:53:35 -0600

    Messing around

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+// Library build configuration.
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url';
+
+export default {
+  // Specify entries of your library, so that Rollup
+  // can figure out their inter-dependencies.
+  input: ['src/index.js', 'src/a.js', 'src/b.js'],
+  output: [
+    {
+      // Bundle into ESM for modern consumers.
+      // Only ESM build can currently be tree-shaken.
+      dir: 'build/esm',
+      format: 'esm',
+    },
+    {
+      // Bundle into CJS for other consumers.
+      dir: 'build/cjs',
+      format: 'cjs',
+    },
+  ],
+  plugins: [resolve(), url()],
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// A master-module of our library.
+// Here we control what modules get exported
+// as the public API of the library.
+export { funcA } from './a'
+export { funcB } from './b'
+export { funcC } from './b'
